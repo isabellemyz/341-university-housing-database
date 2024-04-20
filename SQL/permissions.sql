57b7f537c8ce669e58ef039606a02d2c3a3da854
Crystal Zhu <122851454+crystal-zhu@users.noreply.github.com>
2024-04-20 16:28:33 -0400

    Updating permissions for all tables to dbuser

--- SQL/permissions.sql
+++ SQL/permissions.sql
@@ -1,15 +1,54 @@
 -- granting permissions for tables
 use UniversityHousing;
-grant select on amenity to dbuser;
-grant insert on amenity to dbuser;
-grant update on amenity to dbuser;
-grant delete on amenity to dbuser;
+GRANT SELECT ON amenity TO dbuser;
+GRANT INSERT ON amenity TO dbuser;
+GRANT UPDATE ON amenity TO dbuser;
+GRANT DELETE ON amenity TO dbuser;
 
-grant select on building_amenity to dbuser;
-grant insert on building_amenity to dbuser;
-grant update on building_amenity to dbuser;
-grant delete on building_amenity to dbuser;
+GRANT SELECT ON assignment TO dbuser;
+GRANT INSERT ON assignment TO dbuser;
+GRANT UPDATE ON assignment TO dbuser;
+GRANT DELETE ON assignment TO dbuser;
 
+GRANT SELECT ON building_amenity TO dbuser;
+GRANT INSERT ON building_amenity TO dbuser;
+GRANT UPDATE ON building_amenity TO dbuser;
+GRANT DELETE ON building_amenity TO dbuser;
+
+GRANT SELECT ON buildings TO dbuser;
+GRANT INSERT ON buildings TO dbuser;
+GRANT UPDATE ON buildings TO dbuser;
+GRANT DELETE ON buildings TO dbuser;
+
+GRANT SELECT ON groups TO dbuser;
+GRANT INSERT ON groups TO dbuser;
+GRANT UPDATE ON groups TO dbuser;
+GRANT DELETE ON groups TO dbuser;
+
+GRANT SELECT ON maintenance_request TO dbuser;
+GRANT INSERT ON maintenance_request TO dbuser;
+GRANT UPDATE ON maintenance_request TO dbuser;
+GRANT DELETE ON maintenance_request TO dbuser;
+
+GRANT SELECT ON preference TO dbuser;
+GRANT INSERT ON preference TO dbuser;
+GRANT UPDATE ON preference TO dbuser;
+GRANT DELETE ON preference TO dbuser;
+
+GRANT SELECT ON rooms TO dbuser;
+GRANT INSERT ON rooms TO dbuser;
+GRANT UPDATE ON rooms TO dbuser;
+GRANT DELETE ON rooms TO dbuser;
+
+GRANT SELECT ON staff TO dbuser;
+GRANT INSERT ON staff TO dbuser;
+GRANT UPDATE ON staff TO dbuser;
+GRANT DELETE ON staff TO dbuser;
+
+GRANT SELECT ON student TO dbuser;
+GRANT INSERT ON student TO dbuser;
+GRANT UPDATE ON student TO dbuser;
+GRANT DELETE ON student TO dbuser;
 
 -- granting permissions for stored procedures
 use UniversityHousing;
