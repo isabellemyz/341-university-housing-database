cb03fcb9c947e54157d0e5963f101fef131523f5
Crystal Zhu <122851454+crystal-zhu@users.noreply.github.com>
2024-04-20 22:52:09 -0400

    Update permissions.sql

--- SQL/permissions.sql
+++ SQL/permissions.sql
@@ -56,4 +56,5 @@ grant execute on addAmenity to dbuser;
 grant execute on insertStudent to dbuser;
 grant execute on insertPreference to dbuser;
 grant execute on AnalyzeStudentPreferences to dbuser; 
+grant execute on insertValidRequest to dbuser;
 
