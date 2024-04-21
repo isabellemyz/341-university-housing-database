a7ccee7cba082c5d86e9e67791e38a1c0d637dd6
Crystal Zhu <122851454+crystal-zhu@users.noreply.github.com>
2024-04-21 02:48:03 -0400

    crystals java!

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,9 +14,10 @@ import mypackage.Admin;
 import mypackage.Student;
 
 public class App {
-    // want to rework this to prompt user for username (ex: ixz33), database name, user (dbuser), and password
+    // want to rework this to prompt user for username (ex: ixz33), database name,
+    // user (dbuser), and password
     // then generate connectionUrl dynamically
-    private static final String connectionUrl = "jdbc:sqlserver://cxp-sql-03\\txl790;" // PUT YOUR INFO HERE
+    private static final String connectionUrl = "jdbc:sqlserver://cxp-sql-02\\cxz630;" // PUT YOUR INFO HERE
             + "database=UniversityHousing;"
             + "user=dbuser;" // MAKE SURE YOU HAVE dbuser AND CORRECT PASSWORD ON YOUR DB
             + "password=csds341143sdsc;"
@@ -42,21 +44,21 @@ public class App {
                     // insert student
                     System.out.println("Enter student's first name, then press enter: ");
                     String inpFirstName = myObj.nextLine();
-                    
+
                     System.out.println("Enter student's last name, then press enter: ");
                     String inpLastName = myObj.nextLine();
 
                     System.out.println("Enter student's year, then press enter: ");
                     int inpYear = myObj.nextInt();
                     myObj.nextLine();
-                    
+
                     System.out.println("Enter if student is RA (TRUE or FALSE), then press enter: ");
                     boolean inpIsRA = myObj.nextBoolean();
                     myObj.nextLine();
 
                     System.out.println("Enter student's email, then press enter: ");
                     String inpEmail = myObj.nextLine();
-                    
+
                     System.out.println("Enter student's phone number (XXX-XXX-XXXX), then press enter: ");
                     String inpPhoneNumber = myObj.nextLine();
 
@@ -76,24 +78,26 @@ public class App {
                     String inpSpace = myObj.nextLine().toLowerCase();
                     myObj.nextLine();
 
-                    System.out.println("When do you usually go to bed?(enter a number, ex. 8 = 8pm, 1 = 1 am)"); 
-                    int inpSleep = myObj.nextInt(); 
+                    System.out.println("When do you usually go to bed?(enter a number, ex. 8 = 8pm, 1 = 1 am)");
+                    int inpSleep = myObj.nextInt();
                     myObj.nextLine();
 
                     System.out.println(
                             "How do you prefer your living space to be organized?('messy', 'clean', 'lived-in')");
-                    String inpLiving = myObj.nextLine().toLowerCase(); 
+                    String inpLiving = myObj.nextLine().toLowerCase();
                     myObj.nextLine();
 
-                    System.out.println("How many roommates are you comfortable living with?(enter a number between 1 to 9)");
-                    int inpRoommate = myObj.nextInt(); 
+                    System.out.println(
+                            "How many roommates are you comfortable living with?(enter a number between 1 to 9)");
+                    int inpRoommate = myObj.nextInt();
 
                     myObj.close();
 
-                    int inpId = Student.insertStudent(inpFirstName, inpLastName, inpYear, inpIsRA, inpEmail, inpPhoneNumber,
+                    int inpId = Student.insertStudent(inpFirstName, inpLastName, inpYear, inpIsRA, inpEmail,
+                            inpPhoneNumber,
                             connectionUrl);
-                    Student.insertPreference(inpId, inpSmoke, inpMusic, inpSpace, inpSleep, inpLiving, inpRoommate, connectionUrl);
-
+                    Student.insertPreference(inpId, inpSmoke, inpMusic, inpSpace, inpSleep, inpLiving, inpRoommate,
+                            connectionUrl);
 
                     break;
                 case 1:
@@ -113,9 +117,48 @@ public class App {
                             "Please enter an action:\n\t0:Submit a new request\n\t1:Check my maintenance progress");
                     int maintenanceAction = myObj.nextInt();
                     if (maintenanceAction == 0) {
-                        // insertMaintenance
+                        // insert maintenance request
+                        System.out.println("Enter your student ID, then press enter: ");
+                        int inpSID= myObj.nextInt();
+
+                        int inpStaffID = -1;
+                        int inpBuildingID = -1;
+                        int inpAmenityID=-1;
+                        int inpRoomNum=-1;
+
+                        System.out.println(
+                            "Please enter where your maintenance request will be for:\n\t0: Your Room\n\t1: Building-wide Amenity");
+                       
+                            int maintenanceLocation = myObj.nextInt();
+                            if (maintenanceLocation == 0) {
+                                System.out.println("Enter your building's ID, then press enter: ");
+                                inpBuildingID = myObj.nextInt();
+                                System.out.println("Enter your room number, then press enter: ");
+                                inpRoomNum = myObj.nextInt();
+                            }
+                            else if (maintenanceLocation == 1) {
+                                System.out.println("Enter your building's ID, then press enter: ");
+                                inpBuildingID = myObj.nextInt();
+                                System.out.println("Enter the ID of the amenity in your building, then press enter: ");
+                                inpAmenityID = myObj.nextInt();
+                            }
+                            
+                        String inpStatus = "submitted";
+                        Timestamp inpDateSub = new Timestamp(System.currentTimeMillis());
+                        Timestamp inpDateComp = null;
+
+                        myObj.nextLine();
+
+                        myObj.close();
+
+                        Student.insertValidRequest(inpSID,inpStaffID,inpBuildingID,inpRoomNum,inpAmenityID, inpStatus, inpDateSub, inpDateComp, connectionUrl);
+                        
                     } else if (maintenanceAction == 1) {
-                        // query maintenance
+                        // query database for all requests related to this building_id and amenity_id/room_number and 'submitted' or 'in progress' and show who made the request (name)
+                        System.out.println("Enter your student ID, then press enter: ");
+                        int inpSID= myObj.nextInt();
+
+
                     }
                     break;
                 default:
@@ -124,19 +167,17 @@ public class App {
 
         } else if (userType == 1) { // maintenance staff
             System.out.println(
-                "Please enter an action:\n\t0:"
+                    "Please enter an action:\n\t0:"
 
             ); // change this
 
-
         } else if (userType == 2) { // admin
             System.out.println(
-                "Please enter an action:\n\t0:Update availability information\n\t1:Update amenities\n\t2:Assign groups"
-            );
+                    "Please enter an action:\n\t0:Update availability information\n\t1:Update amenities\n\t2:Assign groups");
 
             int adminAction = myObj.nextInt();
             myObj.nextLine();
-            
+
             switch (adminAction) {
                 case 0:
                     // update availability
@@ -152,7 +193,7 @@ public class App {
 
                     Admin.updateRoomStatus(inpBuildingName, inpRoomNumber, inpAvailability, connectionUrl);
                     break;
-                
+
                 case 1:
                     // update amenities
                     System.out.println("Enter the new amenity name, then press enter: ");
@@ -174,7 +215,8 @@ public class App {
                     int inpCost = myObj.nextInt();
                     myObj.nextLine();
 
-                    System.out.println("Enter the name of the building the amenity will be added to, then press enter: ");
+                    System.out
+                            .println("Enter the name of the building the amenity will be added to, then press enter: ");
                     String inpAmenityBuildingName = myObj.nextLine();
 
                     System.out.println("Enter how many amenities will be added, then press enter: ");
@@ -186,34 +228,39 @@ public class App {
                     Timestamp inpStartTime = null;
                     Timestamp inpEndTime = null;
 
-                    try { 
+                    try {
                         inpStartTime = App.parseTimestamp(startTimeStr);
                         inpEndTime = App.parseTimestamp(endTimeStr);
                     } catch (ParseException e) {
-                        System.err.println("Error: Invalid timestamp format. Please enter timestamp in the format yyyy-MM-dd HH:mm:ss");
+                        System.err.println(
+                                "Error: Invalid timestamp format. Please enter timestamp in the format yyyy-MM-dd HH:mm:ss");
                     }
 
-                    Admin.addAmenity(inpAmenityName, inpCategory, inpStartTime, inpEndTime, inpDescription, inpCost, inpAmenityBuildingName,
-                        inpAmenityCount, connectionUrl);
+                    Admin.addAmenity(inpAmenityName, inpCategory, inpStartTime, inpEndTime, inpDescription, inpCost,
+                            inpAmenityBuildingName,
+                            inpAmenityCount, connectionUrl);
                     break;
-                
+
                 case 2:
                     // assign groups
                     break;
-                
+
                 default:
                     System.out.println("Invalid action.");
             }
-            
+
         } else {
             throw new IllegalArgumentException("Please enter a value between 0 to 2");
         }
         // }
     }
 
-     // helper method to parse timestamps
+    // helper method to parse timestamps
     private static Timestamp parseTimestamp(String timestampStr) throws ParseException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return new Timestamp(dateFormat.parse(timestampStr).getTime());
+    }
+}
+
     }
 }
